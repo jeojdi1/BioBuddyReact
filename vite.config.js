c3754bee7304bb6b7136814ddbef4d2cc1a83fff
jeojdi <135281119+jeojdi@users.noreply.github.com>
2025-06-22 04:22:09 -0400

    fonrt

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BIOBUDDYREACT/', // Match your repository name exactly (case-sensitive)
   build: {
     outDir: 'docs', // Output to docs folder for GitHub Pages
     emptyOutDir: true, // Clear docs folder before each build
